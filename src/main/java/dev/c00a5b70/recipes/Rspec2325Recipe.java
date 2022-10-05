1ade4d8cc2a0a8d555c2dc1b3e595f48c86a6ec5
chris-miner <94078897+chris-miner@users.noreply.github.com>
2022-10-05 14:49:55 -0600

    Initial functional implementation and tests.

--- src/main/java/dev/c00a5b70/recipes/Rspec2325Recipe.java
+++ src/main/java/dev/c00a5b70/recipes/Rspec2325Recipe.java
@@ -1,6 +1,20 @@
 package dev.c00a5b70.recipes;
 
+import java.util.Collections;
+
+import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
+import org.openrewrite.Tree;
+import org.openrewrite.internal.ListUtils;
+import org.openrewrite.internal.lang.Nullable;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.cleanup.ModifierOrder;
+import org.openrewrite.java.tree.Flag;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.java.tree.JavaType;
+import org.openrewrite.java.tree.JavaType.Variable;
+import org.openrewrite.java.tree.Space;
+import org.openrewrite.marker.Markers;
 
 public class Rspec2325Recipe extends Recipe {
 
@@ -11,7 +25,77 @@ public class Rspec2325Recipe extends Recipe {
 
     @Override
     public String getDescription() {
-        return "Non-overridable methods (private or final) that don’t access instance data can be static to prevent any misunderstanding about the contract of the method.";
+        return "Non-overridable methods (private or final) that don't access instance data can be static to prevent any misunderstanding about the contract of the method.";
+    }
+
+    protected JavaIsoVisitor<ExecutionContext> getVisitor() {
+        // getVisitor() should always return a new instance of the visitor to avoid any
+        // state leaking between cycles
+        return new StaticKeywordVisitor();
+    }
+
+    public class StaticKeywordVisitor extends JavaIsoVisitor<ExecutionContext> {
+
+        private J.MethodDeclaration curMethod;
+        private Boolean classVariableUsedInMethod = false;
+        private Boolean instanceVariableUsedInMethod = false;
+
+        @Override
+        public J.Identifier visitIdentifier(J.Identifier identifier, ExecutionContext p) {
+            @Nullable
+            Variable variable = identifier.getFieldType();
+
+            // fieldType is non-null for identifiers that are variable references
+            if (variable != null) {
+                // Check if this is a class variable
+                if (variable.getOwner() instanceof JavaType.Class) {
+                    if (variable.hasFlags(Flag.Static)) {
+                        if (curMethod == null) {
+                            System.out.println("Class variable: " + variable.getName());
+                        } else if (curMethod != null) {
+                            System.out.println("Class variable: " + variable.getName() + " used in method: "
+                                    + curMethod.getSimpleName());
+                            classVariableUsedInMethod = true;
+                        }
+                    } else {
+                        System.out.println("Instance variable: " + variable.getName());
+                        instanceVariableUsedInMethod = true;
+                    }
+                }
+
+                // or maybe a method variable
+                else if (variable.getOwner() instanceof JavaType.Method) {
+                    System.out.println("Method variable: " + variable.getName());
+                }
+
+                // or something else
+                else {
+                    System.out.println("Unknown variable type: " + variable.getName());
+                }
+            }
+
+            return super.visitIdentifier(identifier, p);
+        }
+
+        @Override
+        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext p) {
+            curMethod = method;
+            Boolean isPrivateOrFinal = (method.hasModifier(J.Modifier.Type.Private)
+                    || method.hasModifier(J.Modifier.Type.Final));
+
+            J.MethodDeclaration md = super.visitMethodDeclaration(method, p);
+            // Check if class variables were used in this method
+            if (!instanceVariableUsedInMethod) {
+                if (isPrivateOrFinal && !md.hasModifier(J.Modifier.Type.Static)) {
+                    J.Modifier mod = new J.Modifier(Tree.randomId(), Space.EMPTY, Markers.EMPTY, J.Modifier.Type.Static,
+                            Collections.emptyList());
+                    md = md.withModifiers(ModifierOrder.sortModifiers(ListUtils.concat(mod, md.getModifiers())));
+                    md = autoFormat(md, md, p, getCursor().getParent());
+                }
+            }
+            return md;
+        }
+
     }
 
 }
--- src/test/java/dev/c00a5b70/recipes/Rspec2325RecipeTest.java
+++ src/test/java/dev/c00a5b70/recipes/Rspec2325RecipeTest.java
@@ -3,8 +3,6 @@ package dev.c00a5b70.recipes;
 import static org.openrewrite.java.Assertions.java;
 
 import org.junit.jupiter.api.Test;
-import org.openrewrite.java.JavaParser;
-import org.openrewrite.starter.NoGuavaListsNewArrayList;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
@@ -12,36 +10,45 @@ public class Rspec2325RecipeTest implements RewriteTest {
 
   @Override
   public void defaults(RecipeSpec spec) {
-    spec
-        .recipe(new NoGuavaListsNewArrayList())
-        .parser(JavaParser.fromJavaVersion()
-            .logCompilationWarningsAndErrors(true)
-            .classpath("guava"));
+    spec.recipe(new Rspec2325Recipe());
   }
 
   @Test
-  void privateMethodWithClassDataAccessIsStatic() {
-    rewriteRun(spec -> spec
-        .parser(JavaParser.fromJavaVersion()
-            .logCompilationWarningsAndErrors(false)
-            .classpath("guava")),
+  void privateMethodNoInstanceDataIsStatic() {
+    rewriteRun(
         java(
             """
-                class TestClass {
-                  private static String classData = "magic";
-
-                  private String method() {
-                    return classData;
-                  }
+                class SampleClass {
+                    private Boolean sampleMethod() {
+                        return true;
+                    }
                 }
                 """,
             """
-                 class TestClass {
-                  private static String classData = "magic";
+                class SampleClass {
+                    private static Boolean sampleMethod() {
+                        return true;
+                    }
+                }
+                """));
+  }
 
-                  private static String method() {
-                    return classData;
-                  }
+  @Test
+  void finalMethodNoInstanceDataIsStatic() {
+    rewriteRun(
+        java(
+            """
+                class SampleClass {
+                    final Boolean sampleMethod() {
+                        return true;
+                    }
+                }
+                """,
+            """
+                class SampleClass {
+                    static final Boolean sampleMethod() {
+                        return true;
+                    }
                 }
                 """));
   }
